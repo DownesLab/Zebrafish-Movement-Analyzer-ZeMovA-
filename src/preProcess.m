4d68146dad8644d840c17a02110602e79534fdc8
Abhay Mittal <mittalabhay94@gmail.com>
2016-12-01 15:34:25 -0500

    PreProcess => remove rgb2gray, doing it outside

--- src/preProcess.m
+++ src/preProcess.m
@@ -6,7 +6,7 @@ function [Img] = preProcess(Img, bThresh, minPixel)
 %   noise and autoremoval of probe). Img is the image, bThresh is the
 %   threshold used for converting the image to binary and minPixel is the
 %   minimum number of pixels that any connected component must have
-    Img=rgb2gray(Img);
+%     Img=rgb2gray(Img);
     Img=imcomplement(Img); 
     Img=imclearborder(Img,4);
     Img=imcomplement(Img);
