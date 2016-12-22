7161deba9fa9078871db27113dc61974c0b6e99c
Abhay Mittal <mittalabhay94@gmail.com>
2016-12-22 14:51:12 -0500

    Basic detection of 3 points

--- src/capturePoints.m
+++ src/capturePoints.m
@@ -1,6 +1,6 @@
 h=figure('Position', [100, 00, 1300, 1000]);
 prefix='a';
-for  i = [1:10]
+for  i = [1:5]
    videoSource = VideoReader(strcat('../Mittal share/',prefix,'_',int2str(i),'.avi'));
 %    videoPlayqer = vision.VideoPlayer('Position', [100, 100, 1000, 1000]);
    k=1;
@@ -39,10 +39,15 @@ for  i = [1:10]
 %         Draw the skeleton
         
         I=preProcess(I,0.955,140);
-        subplot(2,3,6),subimage(I),title('preprocessed');
+%         subplot(2,3,6),subimage(I),title('preprocessed');
 %         I=fillSmallHoles(I);
         se=strel('disk',1);
         I=imclose(I,se);
+        
+        badSkel=bwmorph(I,'skel',Inf);
+        subplot(2,3,5),subimage(imadd(originalI,im2double(badSkel))),title('originalSkel');
+        
+        
         skel=bwmorph(skeleton(I)>25,'skel',Inf);
         k=imadd(originalI,im2double(skel));
         subplot(2,3,3),subimage(k),title('skeleton');
@@ -51,12 +56,18 @@ for  i = [1:10]
         subplot(2,3,4),subimage(k),title('joined');
         
         
-        
+        mask=zeros(size(originalI));
         hp=headPoint(head,skel,I);
+        [bdRC,tailRC]=detectBodyTail(hp,skel,I,0.4);
         z=originalI;
-        z(hp(1,1),hp(1,2))=1;
-        subplot(2,3,5),subimage(z);
-        pause(5);
+        mask(hp(1,1),hp(1,2))=1;
+        mask(tailRC(1,1),tailRC(1,2))=1;
+        mask(bdRC(1,1),bdRC(1,2))=1;
+        se=strel('square',7);
+        mask=imdilate(mask,se);
+        subplot(2,3,6),subimage(imadd(z,im2double(mask)));
+        
+        pause(1);
 %                 pause(1);
         
         for j=[1:20]
@@ -66,12 +77,10 @@ for  i = [1:10]
         end
 %                 saveas(h,strcat('custom/',prefix,int2str(i),'_',int2str(k),'.jpg'));  
                 k=k+1;
-           end
+   end
 end
 function hp=headPoint(headRegion,skel,img)
     [dmap, endCR, branchCR]=anaskel(skel);
-    endPoints=zeros(size(skel));
-    endPoints(endCR)=1;
 %     approach is to compute the geodesic distance transform with
 %     headRegion as seed and then find the endpoint with the min value
     [headRgnRow, headRgnCol]=find(headRegion);
@@ -87,17 +96,37 @@ function hp=headPoint(headRegion,skel,img)
         end
     end
     
-    D=bwdistgeodesic(img,[headRC(1,2)],[headRC(1,1)]);
-    headRC=[0 0];
-    minDist=Inf;
-    for j = [1:size(headRgnRow)]
-        if D(headRgnRow(i),headRgnCol(i))<minDist
-            minDist=D(headRgnRow(i),headRgnCol(i));
-            headRC(1,1)=headRgnRow(i);
-            headRC(1,2)=headRgnCol(i);
+%     D=bwdistgeodesic(img,[headRC(1,2)],[headRC(1,1)]);
+%     headRC=[0 0];
+%     minDist=Inf;
+%     for j = [1:size(headRgnRow)]
+%         if D(headRgnRow(i),headRgnCol(i))<minDist
+%             minDist=D(headRgnRow(i),headRgnCol(i));
+%             headRC(1,1)=headRgnRow(i);
+%             headRC(1,2)=headRgnCol(i);
+%         end
+%     end
+    hp=headRC;
+end
+
+function [bodyRC,tailRC]=detectBodyTail(headPoint,skel,img,proportion)
+    [dmap,endCR,branchCR]=anaskel(skel);  
+    
+    tailRC=[0 0];
+     D=bwdistgeodesic(skel,headPoint(1,2),headPoint(1,1));
+    if size(endCR,2)>1
+        maxDist=-Inf;
+        for i = [1:size(endCR,2)]
+            if D(endCR(2,i),endCR(1,i))>maxDist
+                maxDist=D(endCR(2,i),endCR(1,i));
+                tailRC(1,1)=endCR(2,i);
+                tailRC(1,2)=endCR(1,i);
+            end
         end
     end
-    hp=headRC;
+    
+    bodyRC=[0 0];
+    [bodyRC(1,1), bodyRC(1,2)]=find(D==20);
 end
 function fillImg=fillSmallHoles(img)
     filled = imfill(img,'holes');
